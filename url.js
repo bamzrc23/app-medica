3bcc13f5a0409e9bf090338bb76be9615e08c314
Bryan Alexander Matailo <108232957+bamzrc23@users.noreply.github.com>
2024-08-01 10:48:52 -0500

    Update url.js

--- url.js
+++ url.js
@@ -1,5 +1,2 @@
-export const url = "https://backend-production-2615.up.railway.app/"
-
-//https://backend-production-2615.up.railway.app/
-
-//http://192.168.0.108:4000/
+export const url = "http://192.168.0.0:4000/"
+//tu direccion ip para manejar el backend localmente
